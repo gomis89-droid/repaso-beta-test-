cab24fc02fa644b3893d7c4cf1e356b5fe068a6a
gomis89-droid <gomis89@gmail.com>
2026-04-19 04:40:26 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = 'mode-help-safe-v1';
+const CACHE_VERSION = 'registro-mobile-tabs-v3';
 const CORE_ASSETS = ['./','./index.html','./manifest.json','./icons/icon-192.png','./icons/icon-512.png'];
 
 self.addEventListener('install', event => {
