5737e5b521dcf5da9fb08c358318841f86a4d174
gomis89-droid <gomis89@gmail.com>
2026-04-19 04:25:42 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = 'mode-help-fix-v1';
+const CACHE_VERSION = 'mode-help-safe-v1';
 const CORE_ASSETS = ['./','./index.html','./manifest.json','./icons/icon-192.png','./icons/icon-512.png'];
 
 self.addEventListener('install', event => {
